7aa64c62e62da49b9d1f2b64c4a17a4d4ca6e73e
David <dhahn123@gmail.com>
2020-10-20 16:13:27 -0700

    Set up AuthorList.tsx, reorder.ts, types.ts, install @types/modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,40 @@
 import * as React from "react";
-import './App.css';
+import { DragDropContext } from "react-beautiful-dnd";
+import { AuthorList } from "./AuthorList";
+import { reorderColors } from "./reorder";
+import { ColorMap } from './types';
 
 const App = () => {
-  const [colors, setColors] = React.useState({
+  const [colorMap, setColors] = React.useState<ColorMap>({
     a: ["blue", "red"],
     b: ["pink"],
     c: ["green"]
   });
 
-  return <div>hello</div>;
+  return (
+    <DragDropContext onDragEnd={({ destination, source }) => {
+      // // dropped outside the list
+      if (!destination) {
+        return;
+      }
+
+      setColors(reorderColors(colorMap, source, destination));
+    }}
+    >
+
+      <div>
+        {Object.entries(colorMap).map(([k, v]) => (
+          <AuthorList
+            internalScroll
+            key={k}
+            listId={k}
+            listType="CARD"
+            colors={v}
+          />
+        ))}
+      </div>
+    </DragDropContext>
+    );
 }
 
 export default App;
